77243bddb0ba0c14040ec1bdb9c9e6b9c1f7f6d8
Gaurav Chaudhary <jaatsaab735@gmail.com>
2024-12-16 11:35:42 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000,      // Set the port to 10000 for Render
+    host: '0.0.0.0',  // Ensure it's accessible from outside
+  },
 })
