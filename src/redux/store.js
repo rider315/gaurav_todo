841942625c5a6e345d3dfdbc8fae17a04dba9769
Gaurav Chaudhary <jaatsaab735@gmail.com>
2024-12-16 10:43:34 +0530

    ok

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,63 @@
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { thunk } from 'redux-thunk';
+
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import { todosReducers } from './reducers/todosReducer';
+// import { tabReducer } from './reducers/tabReducer';
+// // import { tabReducer } from './reducers/tabReducer';
+
+// const reducer = combineReducers({
+//     todos: todosReducers,
+//     currentTab:tabReducer
+    
+// })
+
+
+
+
+
+
+
+
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//     reducer,
+//     composeWithDevTools(applyMiddleware(...middleware))
+// )
+
+// export default store;
+
+
+
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-
-import { composeWithDevTools } from 'redux-devtools-extension';
-
+import thunk from 'redux-thunk'; // Fixed import of thunk
 import { todosReducers } from './reducers/todosReducer';
 import { tabReducer } from './reducers/tabReducer';
-// import { tabReducer } from './reducers/tabReducer';
 
+// Combine reducers
 const reducer = combineReducers({
     todos: todosReducers,
-    currentTab:tabReducer
-    
-})
+    currentTab: tabReducer
+});
 
-
-
-
-
-
-
-
-
+// Setup middleware
 const middleware = [thunk];
 
+// Use composeWithDevTools only in development
+let composeEnhancers = (f) => f;
+
+if (process.env.NODE_ENV === 'development') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    composeEnhancers = composeWithDevTools;
+}
+
+// Create store
 const store = createStore(
     reducer,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+    composeEnhancers(applyMiddleware(...middleware))
+);
 
 export default store;
